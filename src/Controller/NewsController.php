0d2e8d2417626b7b6dccb9e70a0533ca1a25601b
Sanction <sayou.jeremie@gmail.com>
2019-03-26 15:33:49 +0100

    Ajout formulaire, validations

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -4,8 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
 use App\Entity\News;
 use App\Repository\NewsRepository;
+use App\Form\NewsType;
 
 class NewsController extends AbstractController
 {
@@ -15,6 +19,7 @@ class NewsController extends AbstractController
     public function index(NewsRepository $repo)
     {
         $articles = $repo->findAll();
+        
         return $this->render('news/index.html.twig', [
             'controller_name' => 'NewsController',
             'articles' => $articles
@@ -26,6 +31,38 @@ class NewsController extends AbstractController
      */
     public function home() {
         return $this->render('news/home.html.twig');
+    }
+
+    /**
+     * @Route("/news/new", name="news_create")
+     * @Route("/news/{id}/edit", name="news_edit")
+     */
+    public function form(News $article = null, Request $request, ObjectManager $manager) {
+        if (!$article) {
+            $article = new News();
+        }
+
+        $form = $this->createForm(NewsType::class, $article);
+
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            if(!$article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('news_show', [
+                'id' => $article->getId()
+            ]);
+        }
+
+        return $this->render('news/create.html.twig', [
+            'formNews' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
     }
 
     /**
